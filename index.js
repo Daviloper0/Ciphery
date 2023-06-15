5a6c5559ff43d00b5bf616a4b8909366c7cdebfc
Davi de Almeida Guimarães <odaviloper@gmail.com>
2023-06-15 16:16:42 -0300

    Adicionada função de gerar senha

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 import { PrimaryButton, SecondaryButton, TertiaryButton } from "./buttons.js"; 
 import { LongInput, ShortInput } from "./input.js";
+//TODO: AUTO GENERATE PASSWORD WHEN ENTER WEBSITE
 
+function generatePassword() {
+  let passwordInput = document.querySelector('app-longinput#nonEncryptedPassword > input');
+  let passwordLenght = document.querySelector('app-shortinput > input').value === '' ? 8 : document.querySelector('app-shortinput > input').value;
+  let options = document.querySelectorAll('app-secondarybutton[data-set="options"][data-selected="true"]') ?? document.querySelector('app-secondarybutton[data-set="options"]');
+  let finalPassword = "";
+  for (let i = 0; i < passwordLenght; i++) {
+    let randomChar = getRandomCharacter(options);
+    finalPassword += String.fromCharCode(randomChar);
+  }
+  passwordInput.value = finalPassword;
+}
+function getRandomCharacter(options) {
+  let randomOption = getRandomNumber(0, options.length);
+  let minChar = options[randomOption].dataset.start;
+  let maxChar = options[randomOption].dataset.end;
+  let randomChar = getRandomNumber(minChar, maxChar);
+  return randomChar;
+}
+function getRandomNumber(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min) + min);
+}
+document.querySelector('app-primarybutton#generate').addEventListener('click', () => {generatePassword()})
 customElements.define('app-primarybutton', PrimaryButton);
 customElements.define('app-secondarybutton', SecondaryButton);
 customElements.define('app-tertiarybutton', TertiaryButton);
